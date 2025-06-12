fc40f1f95bcdb7f25327d3226b6ca2b31a626633
Nishit956 <nishitdalal25@gmail.com>
2025-06-12 19:03:08 +0530

    Ecommerce-App Completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import { Route, Switch } from 'wouter';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import Shop from './Pages/Shop';
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
+function App() {  
+  return (
+    <div>
+       <Navbar/>
+      <Switch>
+      <Route path="/" component={Shop}></Route>
+      <Route path="/mens"> {<ShopCategory banner ={men_banner} category="men"/>}</Route>
+      <Route path="/womens"> {<ShopCategory banner={women_banner} category="women"/>}</Route>
+      <Route path="/kids"> {<ShopCategory banner ={kid_banner} category="kid"/>}</Route>
+      <Route path="/product" nest>
+        <Route path="/:productId" component={Product}/>
+      </Route>
+      <Route path="/cart"> {<Cart/>} </Route>
+      <Route path="/login"> {<LoginSignup/>}</Route>
+      </Switch>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>An e-commerce website is an online platform that enables businesses to sell products or services to customers, facilitating transactions through the internet, such as browsing, adding items to a cart, and making purchases                
+            </p>
+            <p>
+            E-commerce is an easy-to-launch and suitable-for-most business idea, which has gained the attention of entrepreneurs of all ages in the country. From local shop owners to huge organizations, everyone is looking to create a presence in the online business world. The future of e-commerce can be considered to be bright, and the best time to enter the domain is now!
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
