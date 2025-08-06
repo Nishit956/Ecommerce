7cf44e2cfdf5ab0f28dae5600b1d4c8a92e1d90e
Nishit956 <nishitdalal25@gmail.com>
2025-08-06 14:48:44 +0530

    change in RelatedProduct.jsx

--- frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './RelatedProducts.css'
-import data_product from '../Assets/data'
 import Item from '../Item/Item'
 
 const RelatedProducts = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/allproducts`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((err) => console.error('Error fetching products:', err))
+  }, [])
+
   return (
     <div className='relatedproducts'>
       <h1>Related Products</h1>
       <hr />
       <div className="relatedproducts-item">
-        {data_product.map((item,i) => {
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-        })}
+        {products.map((item, i) => (
+          <Item
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
       </div>
     </div>
   )
