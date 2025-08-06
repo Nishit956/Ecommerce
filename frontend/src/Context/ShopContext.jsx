1a49afc5fd3a065aa7e03be496c97762a551d779
Nishit956 <nishitdalal25@gmail.com>
2025-08-06 14:36:39 +0530

    change in ShopContext.jsx

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -111,14 +111,11 @@ const ShopContextProvider = (props) => {
 
 
     const getTotalCartItems = () => {
-        let totalItem = 0;
-        for (const item in cartItems) {
-            if (cartItems[item] && cartItems[item] > 0) {
-                totalItem += cartItems[item];
-            }
-        }
-        return totalItem;
+        return Object.values(cartItems).reduce((total, qty) => {
+            return qty > 0 ? total + qty : total;
+        }, 0);
     };
+
 
 
     const contextValue = {getTotalCartItems,getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
